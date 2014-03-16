410436c135888202ff6cd677157760f0d06b1212
Matt Lockyer <mattdlockyer@gmail.com>
2014-03-16 14:04:43 -0700

    fixed params

--- gau.js
+++ gau.js
@@ -25,11 +25,11 @@ var GAU = (function(gau) {
 	* Setup
 	*******************************/
 
-	var setup = function(acct, isNew, isSSL) {
+	var setup = function(acct, useUniversal, useSSL) {
 		var prefix = 'http://www.';
-		if (isSSL) prefix = 'https://ssl.';
+		if (useSSL) prefix = 'https://ssl.';
 
-		if (isNew) {
+		if (useUniversal) {
 			(function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
 				(i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
 				m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
